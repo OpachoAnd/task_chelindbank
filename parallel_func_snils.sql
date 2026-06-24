bb48e0283cac2ed7f09c589a78cf5f4b3673826b
PchelinAndrey <andrej.pchelin@gmail.com>
2026-06-25 04:34:30 +0500

    update

---
+++ parallel_func_snils.sql
@@ -0,0 +1,28 @@
+create or replace function func_task_chelindbank(snils_number INT, control_number INT)
+returns text AS $$
+declare 
+	sum_number int := 0;
+	digit TEXT;
+	n_position int := LENGTH(snils_number :: TEXT); 
+begin
+	-- Проверка длины номера и контрольного числа 
+	IF LENGTH(snils_number :: TEXT) <> 9 OR
+	LENGTH(control_number :: TEXT) <> 2
+	THEN
+        RAISE EXCEPTION 'Ошибка: некорретный ввод СНИЛС';
+    END IF;
+	
+	-- Бежим по числам номера СНИЛС
+	FOR i IN 1..LENGTH(snils_number::TEXT) LOOP
+		digit := substring(snils_number::TEXT, i, 1);
+		sum_number = sum_number + digit :: INT * n_position;
+		n_position = n_position - 1;
+	END LOOP;
+
+	IF sum_number <> control_number 
+	THEN
+		return 'Ошибка: Контрольное число неверно';
+	ELSE
+		return 'Успешно: Контрольное число верно';
+	END IF;
+end $$ LANGUAGE plpgsql PARALLEL SAFE;
